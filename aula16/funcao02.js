b4769560bdd69d0cef7cb74af005f59d89f76df3
Isabella <isaph2602@gmail.com>
2024-02-22 18:26:59 -0300

    Funções: parâmetros e ações

---
+++ aula16/funcao02.js
@@ -0,0 +1,9 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+/*
+Se tiver faltando um dos valores, basta adicionar n1=0 e n2=0 nos parâmetros para não constar como undefined! 
+*/
+
+console.log(soma(9, 9))
